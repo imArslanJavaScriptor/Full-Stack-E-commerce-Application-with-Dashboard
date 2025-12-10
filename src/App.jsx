085370e1b2a60efd6cdb41c54e542c03684634cc
Arslan <imarslan7061@gmail.com>
2025-12-10 16:56:23 +0500

    init

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='min-h-screen w-full flex items-center justify-center bg-gray-900 text-white'>App</div>
+  )
+}
+
+export default App
