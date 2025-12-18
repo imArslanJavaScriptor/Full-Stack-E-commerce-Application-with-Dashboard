11747735750ab3e20c072af3b910189473cae0f2
Arslan <imarslan7061@gmail.com>
2025-12-18 18:34:58 +0500

    new

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div className='min-h-screen w-full flex items-center justify-center bg-gray-900 text-white'>App</div>
+    <div className='min-h-screen w-full flex items-center justify-center bg-gray-900 text-white'>App Updated</div>
   )
 }
 
