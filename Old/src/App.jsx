40b97f1902a9147c714ddc7b47c5963e11113d73
Arslan <imarslan7061@gmail.com>
2025-12-18 18:45:00 +0500

    Remove old project files and add root gitignore

--- Old/src/App.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const App = () => {
-  return (
-    <div className='min-h-screen w-full flex items-center justify-center bg-gray-900 text-white'>App Updated</div>
-  )
-}
-
-export default App
